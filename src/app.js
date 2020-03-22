b66d40944f734e6bc461ac201b5805970c70a39c
julien <wawrzyniakjulien@gmail.com>
2020-03-22 21:48:35 +0100

    init

---
+++ src/app.js
@@ -0,0 +1,18 @@
+console.log('ahah')
+
+WebAssembly.instantiateStreaming(fetch('../build/main.wasm'), {
+	ethereum: {
+		callDataSize: function () {
+			console.log('callDataSize -> 32')
+			return '9993021aed09375dc6b20l050d242d1611af97ee4a6e93cad'.length / 2
+		},
+		return: function (offset, length) {
+			console.log('return(' + offset + ',' + length + ')')
+		},
+	},
+})
+	.then((result) => {
+		const exports = result.instance.exports
+		exports.main()
+	})
+	.catch(console.error)
